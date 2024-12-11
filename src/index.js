0c40582aa0bc417b28085f3bb0498c9e9136f1e4
SonnieCodes <mgathoni.gathoni9@gmail.com>
2024-12-11 05:09:56 +0300

    form with json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
-  
-}
 
+    // Add event listeners to capture form data and override a form's default behavior
+   
+    const inputForm = document.querySelector("form");
+    
+    // Fetch data based on what the user types into that form
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        // The event object actually contains the value we need
+        // We can select the specific DOM element and get its value
+        const input = document.querySelector("input#searchByID");
+        console.log(input.value);
+    
+        // Display that data on the page
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const title = document.querySelector("section#movieDetails h4");
+            const summary = document.querySelector("section#movieDetails p");
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+    
+    });
+}
 document.addEventListener('DOMContentLoaded', init);
